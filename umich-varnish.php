0c937fbe36babbfee01bcdc9cfbd1ec17ed9f1ac
Patrick Springstubbe <stubbe@umich.edu>
2024-06-12 13:11:21 -0400

    Update wp_remote_request - add url filter - add host header Update updater library

--- umich-varnish.php
+++ umich-varnish.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Plugin Name: U-M: Varnish Cache
+ * Plugin Name: University of Michigan: Varnish Cache
  * Plugin URI: https://github.com/umdigital/umich-varnish/
  * Description: Provides varnish cache purging functionality.
- * Version: 1.3.4
- * Author: U-M: Digital
+ * Version: 1.3.5
+ * Author: U-M: OVPC Digital
  * Author URI: http://vpcomm.umich.edu
+ * Update URI: https://github.com/umdigital/umich-varnish/releases/latest
  */
 
 define( 'UMVARNISH_PATH', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
@@ -15,38 +16,26 @@ include UMVARNISH_PATH .'includes'. DIRECTORY_SEPARATOR .'override.php';
 class UMVarnish {
     static public function init()
     {
-        // UPDATER SETUP
-        if( !class_exists( 'WP_GitHub_Updater' ) ) {
-            include_once UMVARNISH_PATH .'includes'. DIRECTORY_SEPARATOR .'updater.php';
+        // load updater library
+        if( file_exists( UMVARNISH_PATH . implode( DIRECTORY_SEPARATOR, [ 'vendor', 'umdigital', 'wordpress-github-updater', 'github-updater.php' ] ) ) ) {
+            include UMVARNISH_PATH . implode( DIRECTORY_SEPARATOR, [ 'vendor', 'umdigital', 'wordpress-github-updater', 'github-updater.php' ] );
         }
-        if( isset( $_GET['force-check'] ) && $_GET['force-check'] && !defined( 'WP_GITHUB_FORCE_UPDATE' ) ) {
-            define( 'WP_GITHUB_FORCE_UPDATE', true );
+        else if( file_exists( UMVARNISH_PATH .'includes'. DIRECTORY_SEPARATOR .'github-updater.php' ) ) {
+            include UMVARNISH_PATH .'includes'. DIRECTORY_SEPARATOR .'github-updater.php';
         }
-        if( is_admin() ) {
-            new WP_GitHub_Updater(array(
-                // this is the slug of your plugin
-                'slug' => plugin_basename(__FILE__),
-                // this is the name of the folder your plugin lives in
-                'proper_folder_name' => dirname( plugin_basename( __FILE__ ) ),
-                // the github API url of your github repo
-                'api_url' => 'https://api.github.com/repos/umdigital/umich-varnish',
-                // the github raw url of your github repo
-                'raw_url' => 'https://raw.githubusercontent.com/umdigital/umich-varnish/master',
-                // the github url of your github repo
-                'github_url' => 'https://github.com/umdigital/umich-varnish',
-                 // the zip url of the github repo
-                'zip_url' => 'https://github.com/umdigital/umich-varnish/zipball/master',
-                // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-                'sslverify' => true,
-                // which version of WordPress does your plugin require?
-                'requires' => '3.0',
-                // which version of WordPress is your plugin tested up to?
-                'tested' => '3.9.1',
-                // which file to use as the readme for the version number
-                'readme' => 'README.md',
-                // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
-                'access_token' => '',
-            ));
+
+        // Initialize Github Updater
+        if( class_exists( '\Umich\GithubUpdater\Init' ) ) {
+            new \Umich\GithubUpdater\Init([
+                'repo' => 'umdigital/umich-varnish',
+                'slug' => plugin_basename( __FILE__ ),
+            ]);
+        }
+        // Show error upon failure
+        else {
+            add_action( 'admin_notices', function(){
+                echo '<div class="error notice"><h3>WARNING</h3><p>U-M: Varnish Cacche plugin is currently unable to check for updates due to a missing dependency.  Please <a href="https://github.com/umdigital/umich-varnish">reinstall the plugin</a>.</p></div>';
+            });
         }
 
         // IF LOGGED IN COOKIE AND COOKIE STALE (not logged in), LOGOUT
@@ -218,11 +207,16 @@ class UMVarnish {
             }
         }
 
+        $host = parse_url( $url, PHP_URL_HOST );
+
         $res = wp_remote_request(
-            $url,
+            apply_filters( 'umich_varnish_purge_url', $url, $host ),
             array(
                 'method'         => 'PURGE',
-                'X-Purge-Method' => $type
+                'headers'        => array(
+                    'Host'           => $host,
+                    'X-Purge-Method' => $type
+                )
             )
         );
 
